a74938b3d2bd384917ed195214390f1dd042c40d
kamlesh-patel1 <kpatel2206@gmail.com>
2019-03-01 01:42:33 +0530

    Thirdcommit

---
+++ Exception/src/nt/exeception/Execption2.java
@@ -0,0 +1,10 @@
+package nt.exeception;
+
+public class Execption2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("exception third commit");
+	}
+
+}
