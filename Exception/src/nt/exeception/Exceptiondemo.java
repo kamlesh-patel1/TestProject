abaf0ce52cdb76c85bb682e3ab5989d519196e99
kamlesh-patel1 <kpatel2206@gmail.com>
2019-03-01 01:32:10 +0530

    second commit

---
+++ Exception/src/nt/exeception/Exceptiondemo.java
@@ -0,0 +1,11 @@
+package nt.exeception;
+
+public class Exceptiondemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("exception");
+
+	}
+
+}
